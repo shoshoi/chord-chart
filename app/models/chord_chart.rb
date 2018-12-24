415ffb06e79f1cf7588b44d5c0f6d80d991cae7e
shoshoi <shoshoiprop@gmail.com>
2018-12-24 15:29:44 +0900

    ChordChartにAddできるようにした

--- app/models/chord_chart.rb
+++ app/models/chord_chart.rb
@@ -5,19 +5,53 @@ class ChordChart
   attr_reader :degree
   attribute :key, Chord, default: nil 
   attribute :scale, String, default: nil
-  attribute :children, Array, default: Array.new
+  attribute :children, Array
 
   def initialize(key="C")
     @key = Chord.get(key)
     @scale = @key.chord_name
+    @children = Array.new
   end 
 
   def add(chord)
-    @children.push chord
+    case chord
+    when Array
+      @children.push chord
+    when String
+      array = chord.split("|").map do |chord_str|
+        chord_str.strip!
+        chords = chord_str.split(" ")
+        if chords.length == 1
+          Chord.get(chords[0])
+        else
+          chart = ChordChart.new(@key.short_name)
+          chart.add(chords.join("|"))
+          chart
+        end
+      end
+      @children.push array
+    end
     @children.flatten!
   end
 
   def root
     @key.root
+  end
+
+  def chart(level=0)
+    chords = @children.map do |child|
+      case child
+      when ChordChart
+        child.chart(level + 1)
+      when Chord
+        child.short_name
+      end
+    end
+    case level
+    when 0
+      chords.join(" | ")
+    when 1
+      chords.join(" ")
+    end
   end
 end
--- spec/models/chord_chart_spec.rb
+++ spec/models/chord_chart_spec.rb
@@ -25,12 +25,34 @@ RSpec.describe ChordChart, type: :model do
     before do
       @chart = ChordChart.new("C")
     end
-    context '引数がChordのとき' do
-      it "仕様未決定" do
-        pending "仕様未決定"
+    context '引数がArray[Chord]のとき' do
+      it "childrenに格納される" do
         @chart.add([Chord.get("C"), Chord.get("D"), Chord.get("E") ,Chord.get("F")])
-        expect(@chart.children).to eq []
+        expect(@chart.children).to eq [Chord.get("C"), Chord.get("D"), Chord.get("E") ,Chord.get("F")]
       end
     end
+    context '引数がStringのとき' do
+      it "childrenに格納される" do
+        @chart.add("C | D | E | F")
+        expect(@chart.children).to eq [Chord.get("C"), Chord.get("D"), Chord.get("E") ,Chord.get("F")]
+      end 
+    end
+    context '引数が入れ子のとき' do
+      it "childrenに格納される" do
+        @chart.add("C G | D | E | F")
+        expect(@chart.chart).to eq "C G | D | E | F"
+      end 
+    end
+  end
+  describe "#chart" do
+    before do
+      @chart = ChordChart.new("C")
+    end 
+    context 'childrenがArray[Chord]のとき' do
+      it "コード進行が出力される" do
+        @chart.add([Chord.get("C"), Chord.get("D"), Chord.get("E") ,Chord.get("F")])
+        expect(@chart.chart).to eq "C | D | E | F"
+      end 
+    end 
   end
 end
